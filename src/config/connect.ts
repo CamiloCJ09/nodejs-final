cfd6da30a82421f15ea7bced4a4ecbff0666323f
CamiloCJ09 <camilo.campaz1@u.icesi.edu.co>
2023-11-15 21:48:24 -0500

    Misc changes

--- src/config/connect.ts
+++ src/config/connect.ts
@@ -1,14 +1,10 @@
-// Dependencies
 import dotenv from 'dotenv';
 import mongoose from 'mongoose';
 
-//enviroment variables
 dotenv.config();
 
-//database connection url 
 const connectionString = process.env.MONGO_CONNECTION_STRING || "mongodb://127.0.0.1:27017/NodeDB";
 
-//database connection
 export const db = mongoose.connect(connectionString).then(() => {
     console.log("Connected to MongoDB");
 }).catch((err) => {
